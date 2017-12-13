1daf6b9e139c73ea62c59eba5f9c439ca5f621e6
AndreVictor60 <andre.victor60@free.fr>
2017-12-13 10:57:54 +0100

    fini

---
+++ user.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 7 Exercice 1</title>
+  </head>
+  <body>
+    <p>Vous êtes sur la page User.php</p>
+    <!-- <?php
+      // if (isset($_GET['firstName']) AND isset($_GET['lastName']))
+      // {
+      //   echo 'Bonjour, ' . $_GET['firstName'] . ' ' . $_GET['lastName'];
+      // }
+     ?> -->
+  </body>
+</html>
