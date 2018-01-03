a12d5c34ef8306608b98b8b96693b7cc1f6a5514
AndreVictor60 <andre.victor60@free.fr>
2018-01-03 10:56:51 +0100

    Commentaire

--- user.php
+++ user.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Partie 7 Exercice 1</title>
-  </head>
-  <body>
-    <p>Vous êtes sur la page User.php</p>
-    <!-- <?php
-      // if (isset($_GET['firstName']) AND isset($_GET['lastName']))
-      // {
-      //   echo 'Bonjour, ' . $_GET['firstName'] . ' ' . $_GET['lastName'];
-      // }
-     ?> -->
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <title>Partie 7 Exercice 1</title>
+    </head>
+    <body>
+        <p>Vous êtes sur la page User.php</p>
+        <?php
+        // if (isset($_GET['firstName']) AND isset($_GET['lastName']))
+        // {
+        //   echo 'Bonjour, ' . htmlspecialchars($_GET['firstName']) . ' ' . htmlspecialchars($_GET['lastName']);
+        // }
+        ?>
+    </body>
 </html>
